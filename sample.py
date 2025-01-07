f89a6619ae52f577339311b0b15c9f19c620e693
Sanjitha <jaggumantri.sanjitha@gmail.com>
2025-01-08 01:28:02 +0530

    Resolved bug #2

--- sample.py
+++ sample.py
@@ -39,6 +39,6 @@ else:
     print("Invalid choice!")
 
  # Change in sample.py
-print("Hello, Git!")
+print("Hello, World!")
 print("This is a change in the main branch.")
 print("This is a change in the feature branch.")
